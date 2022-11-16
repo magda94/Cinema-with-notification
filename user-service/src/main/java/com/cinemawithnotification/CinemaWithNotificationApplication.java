9984ea6738d07e2706645a47b174c315bbb47e67
Magda Wojciak <magdapuscizna@gmail.com>
2022-11-16 19:08:56 +0100

    initial commit

---
+++ user-service/src/main/java/com/cinemawithnotification/CinemaWithNotificationApplication.java
@@ -0,0 +1,13 @@
+package com.cinemawithnotification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaWithNotificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemaWithNotificationApplication.class, args);
+	}
+
+}
---
+++ user-service/src/test/java/com/cinemawithnotification/CinemaWithNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cinemawithnotification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaWithNotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
