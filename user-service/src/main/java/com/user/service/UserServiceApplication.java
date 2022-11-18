56993f0206aa2de4bb53277e88a2f1b1bc94e604
Magda Wojciak <magdapuscizna@gmail.com>
2022-11-18 20:40:08 +0100

    add docker-compose

--- user-service/src/main/java/com/user/service/UserServiceApplication.java
+++ user-service/src/main/java/com/user/service/UserServiceApplication.java
@@ -1,13 +1,13 @@
-package com.cinemawithnotification;
+package com.user.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CinemaWithNotificationApplication {
+public class UserServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CinemaWithNotificationApplication.class, args);
+		SpringApplication.run(UserServiceApplication.class, args);
 	}
 
 }
--- user-service/src/test/java/com/user/service/UserServiceApplicationTests.java
+++ user-service/src/test/java/com/user/service/UserServiceApplicationTests.java
@@ -1,10 +1,10 @@
-package com.cinemawithnotification;
+package com.user.service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CinemaWithNotificationApplicationTests {
+class UserServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
