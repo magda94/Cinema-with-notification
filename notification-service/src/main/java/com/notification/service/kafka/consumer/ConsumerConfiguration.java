4a7ee3e3f313bc487a4dd12a2f9a346298e0268b
Magda Wojciak <magdapuscizna@gmail.com>
2022-12-29 22:01:25 +0100

    dockerfile

--- notification-service/src/main/java/com/notification/service/kafka/consumer/ConsumerConfiguration.java
+++ notification-service/src/main/java/com/notification/service/kafka/consumer/ConsumerConfiguration.java
@@ -4,6 +4,7 @@ import com.notification.service.kafka.dto.CancelNotificationRequest;
 import com.notification.service.kafka.dto.NotificationRequest;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
@@ -19,15 +20,23 @@ import java.util.Map;
 @EnableKafka
 public class ConsumerConfiguration {
 
-    //ToDO get values from yaml
+    @Value("${spring.kafka.consumer.bootstrap-servers}")
+    private String bootstrapServer;
+
+    @Value("${spring.kafka.consumer.group-id}")
+    private String groupId;
+
+    @Value("${spring.kafka.consumer.auto-offset-reset}")
+    private String autoOffsetReset;
+
     @Bean
     public Map<String, Object> consumerConfigs() {
         Map<String, Object> props = new HashMap<>();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:29092");
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer);
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, "notification");
-        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, autoOffsetReset);
         return props;
     }
 
