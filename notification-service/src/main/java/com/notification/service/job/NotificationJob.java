1e2092576c6af3dc73ce0a8a81605b9bf53a1a80
Magda Wojciak <magdapuscizna@gmail.com>
2022-12-29 17:13:45 +0100

    Add job execution and response

--- notification-service/src/main/java/com/notification/service/job/NotificationJob.java
+++ notification-service/src/main/java/com/notification/service/job/NotificationJob.java
@@ -1,15 +1,28 @@
 package com.notification.service.job;
 
+import com.notification.service.kafka.dto.Place;
+import lombok.extern.slf4j.Slf4j;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.springframework.stereotype.Component;
 
+import java.time.Instant;
+
 @Component
+@Slf4j
 public class NotificationJob implements Job {
 
     @Override
     public void execute(JobExecutionContext context) throws JobExecutionException {
-        System.out.println("EXECUTE: " + context.getJobDetail().toString());
+        var data = context.getJobDetail().getJobDataMap();
+
+        var user = data.getString("user");
+        var film = data.getString("film");
+        var startTime = (Instant) data.get("startTime");
+        var place = (Place) data.get("place");
+
+       log.info("TO: '{}' send reminder about film: '{}' which starts at: '{}'. User place: '{}'",
+               user, film, startTime, place);
     }
 }
--- notification-service/src/main/java/com/notification/service/job/NotificationJobService.java
+++ notification-service/src/main/java/com/notification/service/job/NotificationJobService.java
@@ -1,6 +1,7 @@
 package com.notification.service.job;
 
 import com.notification.service.kafka.dto.NotificationRequest;
+import com.notification.service.kafka.dto.ResponseStatus;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.quartz.*;
@@ -15,15 +16,39 @@ import java.time.temporal.ChronoUnit;
 public class NotificationJobService {
 
     private final Scheduler scheduler;
+    private final NotificationAck notificationAck;
 
     private final static int BUFFOR = 15;
 
-    public void createNotificationJob(NotificationRequest request) throws SchedulerException {
-        JobDetail jobDetail = JobBuilder.newJob(NotificationJob.class).build();
+    public void createNotificationJob(NotificationRequest request) {
+        log.info("Sending notification for request: {}", request);
+        JobDetail jobDetail = JobBuilder.newJob(NotificationJob.class)
+                .usingJobData(prepareJobDataMap(request))
+                .build();
+
+
         Trigger trigger = TriggerBuilder.newTrigger()
                 .startAt(Date.from(request.getShowTime().minus(BUFFOR, ChronoUnit.MINUTES)))
                 .build();
 
-        scheduler.scheduleJob(jobDetail, trigger);
+        try {
+            scheduler.scheduleJob(jobDetail, trigger);
+        } catch (Exception ex) {
+            log.error("Job for film : {} and user: {} scheduling failed",
+                    request.getFilmName(), request.getUser(), ex);
+            notificationAck.sendNotificationAck(ResponseStatus.FAILED);
+        }
+        log.info("Notification sent successful");
+        notificationAck.sendNotificationAck(ResponseStatus.SUCCESS);
+
+    }
+
+    private JobDataMap prepareJobDataMap(NotificationRequest request) {
+        JobDataMap jobDataMap = new JobDataMap();
+        jobDataMap.put("user", request.getUser());
+        jobDataMap.put("film", request.getFilmName());
+        jobDataMap.put("startTime", request.getShowTime());
+        jobDataMap.put("place", request.getPlace());
+        return jobDataMap;
     }
 }
