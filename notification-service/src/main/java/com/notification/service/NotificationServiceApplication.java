09d6067c7ed36ee7ee8fa6665707ada50276aef9
Magda Wojciak <magdapuscizna@gmail.com>
2022-12-29 15:54:23 +0100

    quartz with memory in-store

--- notification-service/src/main/java/com/notification/service/NotificationServiceApplication.java
+++ notification-service/src/main/java/com/notification/service/NotificationServiceApplication.java
@@ -1,8 +1,12 @@
 package com.notification.service;
 
+import org.quartz.Scheduler;
+import org.quartz.SchedulerException;
+import org.quartz.impl.StdSchedulerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
@@ -11,5 +15,12 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class NotificationServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(NotificationServiceApplication.class, args);
+    }
+
+    @Bean
+    public Scheduler scheduler() throws SchedulerException {
+        var scheduler = new StdSchedulerFactory().getScheduler();
+        scheduler.start();
+        return scheduler;
     }
 }
